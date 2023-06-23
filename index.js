90f1ebd9153acac1c6304f39ad774de11efa3cc2
Salman Ahmed Siddiqui <120651244+Salman-Ahmed-Siddiqui@users.noreply.github.com>
2023-06-23 16:31:53 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+ const express = require('express');
+ const app = express();
+ const port = process.env.PORT ||3000
+
+ app.get('/', function (req, res) {
+   res.send('Hello, World!');
+ });
+
+ app.listen(port, function () {
+   console.log('Listening on port 3000!');
+ });
